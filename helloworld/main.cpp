b4b342f027c29e89e8e338256fd19600b438cfa7
Kevin Piffero <kevinpiffero@kevins-macbook-air.local>
2018-08-17 14:27:46 -0700

    Initial Commit

---
+++ helloworld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Kevin Piffero on 8/17/18.
+//  Copyright © 2018 Kevin Piffero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
